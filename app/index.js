de540f9fbfa9a1925705e2813382a70b039bea7c
TANMAY SHARMA <123546871+cbof16@users.noreply.github.com>
2024-05-07 01:54:09 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from 'next/navigation';
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "./firebase/config"; 
+import { uath } from "./firebase/config"; 
 
 function Index() {
   const router = useRouter();
@@ -18,7 +18,7 @@ function Index() {
   };
 
   const handleGoogleSignup = () => {
-    const auth = getAuth(auth);
+    const auth = getAuth(uath);
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
